1b0df24202290870840074f6bc8a6a274160e336
CojaONIX <cojaonix@gmail.com>
2024-01-08 11:15:06 +0100

    6.9 home page ocene

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Ocene;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $ocene = Ocene::all();
+    return view('welcome', compact('ocene'));
 });
